d83efd35ebef41be224a670e1e3595f7f9f6deb4
JeremyEwer <jewer@asu.edu>
2014-03-25 23:06:41 -0700

    Create HealthTracker.java

---
+++ src/HealthTracker.java
@@ -0,0 +1,19 @@
+public class HealthTracker
+{
+	public UserAccount currentAccount;
+	public UserAccountList listOfAccounts;
+	
+	public static HealthTracker singleton;
+	
+	public static void main()
+	{
+		singleton = new HealthTracker();
+	}
+	
+	public HealthTracker()
+	{
+		listOfAccounts = new UserAccountList();
+		listOfAccounts.loadAccountList();
+		ChooseAccountMenu CAM = new ChooseAccountMenu();
+	}
+}
