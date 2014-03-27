8ea0513776c9c9405972a464c62b93df5b8b3fde
skylorhaselwood91 <skylor.haselwood@gmail.com>
2014-03-27 14:40:04 -0700

    Create MonthlyRecord.java

---
+++ src/MonthlyRecord.java
@@ -0,0 +1,30 @@
+//CSE 360 Group IV Project "Health Tracker"
+//This class should calculate the users HealthScore based on the average scores for that month for that user and diplay them ou to the user. The user may also choose to print the monthly record out before exiting.
+
+public class MonthlyRecords {
+	
+//Data
+	int HealthScore;
+
+public void displayMonthlyRecordData()
+{
+	return;
+}
+
+
+public int calculateHealthScore()
+{
+	return HealthScore;
+}
+
+public void pringMonthlyRecord()
+{
+	return;
+}
+
+public void gotoMainMenu()
+{
+	return;
+}
+
+}
