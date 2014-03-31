815e5a56c0166bc7e38fc64d4ad3d5309570f1d4
Joshua Stark <JMRante@users.noreply.github.com>
2014-03-30 17:19:53 -0700

    Update HealthTracker.java
    Started to restructure the main HealthTracker class to be functional.

--- src/HealthTracker.java
+++ src/HealthTracker.java
@@ -3,17 +3,10 @@ public class HealthTracker
 	public UserAccount currentAccount;
 	public UserAccountList listOfAccounts;
 	
-	public static HealthTracker singleton;
-	
 	public static void main()
-	{
-		singleton = new HealthTracker();
-	}
-	
-	public HealthTracker()
 	{
 		listOfAccounts = new UserAccountList();
 		listOfAccounts.loadAccountList();
-		ChooseAccountMenu CAM = new ChooseAccountMenu();
+		ChooseAccountMenu CAM = new ChooseAccountMenu();		
 	}
 }
